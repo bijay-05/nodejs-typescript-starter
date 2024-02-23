f076319931402d546e8a85bbef1317e971a9087d
Bijay Kumar Pachhai <86017045+bijay-05@users.noreply.github.com>
2024-02-23 23:13:55 +0545

    Add files via upload

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,28 @@
-import { Router } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Request, Response, Application } from 'express';
 
-import userRoutes from './userRoutes';
+import todorouter from './todoroutes';
+import { notFound } from './middlewares/notFound';
+import { errorHandler } from './middlewares/errorHandler';
 
-const router = Router();
+dotenv.config();
 
-router.use('/user', userRoutes);
+const app: Application = express();
 
-export default router;
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('API is running....');
+});
+
+app.use('/', router);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
